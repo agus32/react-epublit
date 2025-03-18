3a79439a060bef1ef22b93093586ad1f9e12fd52
agus32 <agusluzzini@gmail.com>
2025-03-18 08:12:25 -0300

    fix: unused vars

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import { usePerson } from "../context/PersonContext";
-import { UserIcon } from "./utils";
 
 export const NavBar = () => {
-  const { doLogout } = usePerson();
-
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
